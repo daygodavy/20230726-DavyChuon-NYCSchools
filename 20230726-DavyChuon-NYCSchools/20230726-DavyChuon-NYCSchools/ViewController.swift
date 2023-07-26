4b89ad7aad383b7327203bb367a468a099e1bb04
Davy <dchuon@ucdavis.edu>
2023-07-26 12:16:45 -0700

    removed storyboard and configured scene delegate to initial view

---
+++ 20230726-DavyChuon-NYCSchools/20230726-DavyChuon-NYCSchools/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  20230726-DavyChuon-NYCSchools
+//
+//  Created by Davy Chuon on 7/26/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ 20230726-DavyChuon-NYCSchools/20230726-DavyChuon-NYCSchools/ViewControllers/SchoolListVC.swift
@@ -0,0 +1,17 @@
+//
+//  SchoolListVC.swift
+//  20230726-DavyChuon-NYCSchools
+//
+//  Created by Davy Chuon on 7/26/23.
+//
+
+import UIKit
+
+class SchoolListVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemPink
+    }
+    
+}
